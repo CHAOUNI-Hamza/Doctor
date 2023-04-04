8d6f9246490253bd900795edefc40373351faea0
CHAOUNI-Hamza <chaounihamza19@gmail.com>
2023-04-05 01:23:20 +0200

    sdsq

--- src/views/FrontEnd/DoctorSearch.vue
+++ src/views/FrontEnd/DoctorSearch.vue
@@ -12,48 +12,31 @@
               </div>
               <div class="dashboard-widget">
                 <div class="card-body">
-                  <div class="filter-widget">
+                  <!--<div class="filter-widget">
                     <div class="cal-icon mb-3">
-                      <input
-                        type="date"
-                        class="form-control datetimepicker"
-                        placeholder="Select Date" />
+                      <input type="date" class="form-control datetimepicker" placeholder="Select Date" />
                     </div>
-                  </div>
+                  </div>-->
                   <div class="filter-widget mb-3">
                     <h4>Gender</h4>
                     <div>
                       <label class="custom_check">
-                        <input
-                          type="checkbox"
-                          class="mr-2"
-                          name="gender_type"
-                          value="male" />
+                        <input v-model="params.sex" type="checkbox" class="mr-2" name="gender_type" value="male" />
                         <span class="checkmark"></span> Male Doctor
                       </label>
                     </div>
                     <div>
                       <label class="custom_check">
-                        <input
-                          class="mr-2"
-                          type="checkbox"
-                          name="gender_type"
-                          value="female" />
+                        <input v-model="params.sex" class="mr-2" type="checkbox" name="gender_type" value="female" />
                         <span class="checkmark"></span> Female Doctor
                       </label>
                     </div>
                   </div>
                   <div class="filter-widget mb-3">
                     <h4>Select Specialist</h4>
-                    <div
-                      v-for="speciality in getSpecialities.data"
-                      :key="speciality.id">
+                    <div v-for="speciality in getSpecialities.data" :key="speciality.id">
                       <label class="custom_check">
-                        <input
-                          v-model="params.search_array"
-                          :value="speciality.name"
-                          class="mr-2"
-                          type="checkbox"
+                        <input v-model="params.search_array" :value="speciality.name" class="mr-2" type="checkbox"
                           name="select_specialist" />
 
                         <span class="checkmark"></span> {{ speciality.name }}
@@ -72,10 +55,7 @@
                 <div class="doc-info-left">
                   <div class="doctor-img">
                     <a href="doctor-profile.html">
-                      <img
-                        :src="doctor.photo"
-                        class="img-fluid .img-fluid-pro"
-                        alt="User Image" />
+                      <img :src="doctor.photo" class="img-fluid .img-fluid-pro" alt="User Image" />
                     </a>
                   </div>
                   <div class="doc-info-cont">
@@ -83,16 +63,12 @@
                       <a href="doctor-profile.html">{{ doctor.username }}</a>
                     </h4>
                     <h5 class="doc-department">
-                      <img
-                        :src="doctor.specialty?.photo"
-                        class="img-fluid"
-                        alt="Speciality" />{{ doctor.specialty?.name }}
+                      <img :src="doctor.specialty?.photo" class="img-fluid" alt="Speciality" />{{ doctor.specialty?.name
+                      }}
                     </h5>
                     <div class="clinic-details">
                       <p class="doc-location">
-                        <font-awesome-icon
-                          class="mr-2"
-                          icon="fa-solid fa-location-dot" />
+                        <font-awesome-icon class="mr-2" icon="fa-solid fa-location-dot" />
                         {{ doctor.clinicadresse }}
                       </p>
                     </div>
@@ -105,69 +81,44 @@
                   <div class="clini-infos">
                     <ul style="list-style: none">
                       <li>
-                        <font-awesome-icon
-                          style="width: 29px"
-                          icon="fa-regular fa-heart" />
+                        <font-awesome-icon style="width: 29px" icon="fa-regular fa-heart" />
                         98%
                       </li>
                       <li>
-                        <font-awesome-icon
-                          style="width: 29px"
-                          icon="fa-regular fa-comment-dots" />
+                        <font-awesome-icon style="width: 29px" icon="fa-regular fa-comment-dots" />
                         17 Feedback
                       </li>
                       <li>
-                        <font-awesome-icon
-                          style="width: 29px"
-                          icon="fa-solid fa-location-dot" />
+                        <font-awesome-icon style="width: 29px" icon="fa-solid fa-location-dot" />
                         Florida, USA
                       </li>
                       <li>
-                        <font-awesome-icon
-                          style="width: 29px"
-                          icon="fa-solid fa-sack-dollar" />
+                        <font-awesome-icon style="width: 29px" icon="fa-solid fa-sack-dollar" />
                         $300 - $1000
-                        <font-awesome-icon
-                          style="width: 29px"
-                          icon="fa-solid fa-circle-info" />
+                        <font-awesome-icon style="width: 29px" icon="fa-solid fa-circle-info" />
                       </li>
                     </ul>
                   </div>
                   <div class="clinic-booking">
-                    <a class="view-pro-btn mb-2" href="doctor-profile.html"
-                      >View Profile</a
-                    >
+                    <a class="view-pro-btn mb-2" href="doctor-profile.html">View Profile</a>
                     <a class="apt-btn" href="booking.html">Book Appointment</a>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <nav aria-label="Page navigation example">
+          <nav v-if="getDoctorsLastPage > 1" aria-label="Page navigation example">
             <ul class="pagination">
               <li class="page-item">
-                <a
-                  class="page-link"
-                  @click="params.page = LastPage - 1"
-                  href="#"
-                  >Previous</a
-                >
+                <a class="page-link" @click="params.page = LastPage - 1" href="#">Previous</a>
               </li>
-              <li
-                v-for="LastPage in getDoctorsLastPage"
-                :key="LastPage"
-                class="page-item">
+              <li v-for="LastPage in getDoctorsLastPage" :key="LastPage" class="page-item">
                 <a @click="params.page = LastPage" class="page-link" href="#">{{
                   LastPage
                 }}</a>
               </li>
               <li class="page-item">
-                <a
-                  class="page-link"
-                  @click="params.page = LastPage + 1"
-                  href="#"
-                  >Next</a
-                >
+                <a class="page-link" @click="params.page = LastPage + 1" href="#">Next</a>
               </li>
             </ul>
           </nav>
@@ -191,7 +142,8 @@ export default {
         search_by: "Search by...",
         value: "",
         page: 1,
-        search_array: "",
+        search_array: [],
+        sex: []
       },
     };
   },
@@ -227,6 +179,7 @@ export default {
 .img-fluid-pro {
   border-radius: 9px;
 }
+
 .clinic-booking a {
   background-color: #fff;
   border: 2px solid #0db9f2;
@@ -241,14 +194,17 @@ export default {
   text-transform: uppercase;
   width: 100%;
 }
+
 .clinic-booking a.apt-btn {
   background-color: #0db9f2;
   color: #fff;
   padding: 10px 9px;
 }
+
 .doc-name a {
   color: #20c3f6;
 }
+
 .dashboard-widget .nav-item {
   border-bottom: 1px solid #f0f0f0;
   padding: 9px 20px;
@@ -298,9 +254,11 @@ export default {
   color: #757575;
   font-size: 17px;
 }
+
 .custom_check {
   color: #666;
 }
+
 .card-title {
   float: left;
   font-size: 1.3rem;
@@ -308,10 +266,12 @@ export default {
   margin: 0;
   color: #00000099;
 }
+
 .filter-widget h4 {
   font-size: 18px;
   font-weight: bold;
 }
+
 .card {
   border: 1px solid #f0f0f0;
   margin-bottom: 1.875rem;
@@ -325,31 +285,37 @@ export default {
   border: 1px solid rgba(0, 0, 0, 0.125);
   border-radius: 0.25rem;
 }
+
 .card-body {
   padding: 1.5rem;
 }
+
 .doctor-widget {
   display: -webkit-box;
   display: -ms-flexbox;
   display: flex;
 }
+
 .doc-info-left {
   display: -webkit-box;
   display: -ms-flexbox;
   display: flex;
 }
+
 .doc-info-right {
   margin-left: auto;
   -ms-flex: 0 0 205px;
   flex: 0 0 205px;
   max-width: 205px;
 }
+
 .doctor-img {
   -ms-flex: 0 0 150px;
   flex: 0 0 150px;
   margin-right: 20px;
   width: 150px;
 }
+
 .doc-department img {
   width: 14px;
   height: 14px;
